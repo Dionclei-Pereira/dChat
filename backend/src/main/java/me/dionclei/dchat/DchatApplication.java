1b3817cd6ad3fe1327695395c5df3cbc0655f221
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-05-26 20:38:09 -0300

    Initial commit

---
+++ backend/src/main/java/me/dionclei/dchat/DchatApplication.java
@@ -0,0 +1,13 @@
+package me.dionclei.dchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/me/dionclei/dchat/DchatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dionclei.dchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
