454a32bac195382a546f1c2df8ed284c1dde2178
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-05-27 08:51:09 -0300

    Web Socket config

---
+++ backend/src/main/java/me/dionclei/dchat/configs/WebSocket.java
@@ -0,0 +1,30 @@
+package me.dionclei.dchat.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocket implements WebSocketMessageBrokerConfigurer {
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableStompBrokerRelay("/topic", "/queue")
+			.setRelayHost("rabbitmq")
+			.setRelayPort(61613)
+			.setClientLogin("guest")
+			.setClientPasscode("guest");
+		
+		config.setApplicationDestinationPrefixes("/app");
+	}
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry config) {
+		config.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*")
+			.withSockJS();
+	}
+}
---
+++ backend/src/main/java/me/dionclei/dchat/controllers/ChatController.java
@@ -0,0 +1,8 @@
+package me.dionclei.dchat.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+	
+}
