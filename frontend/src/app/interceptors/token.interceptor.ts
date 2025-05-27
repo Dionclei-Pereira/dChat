92298061c698fd7162914b77a36b757a6fc6f7e3
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-05-27 15:44:37 -0300

    Create token.interceptor.ts

---
+++ frontend/src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class NameInterceptor implements HttpInterceptor {
+
+    constructor(private readonly auth: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const token = this.auth.getToken();
+        if (token) {
+            const clone = req.clone({
+                setHeaders: {
+                    'Authorization': token
+                }
+            })
+            return next.handle(clone);
+        }
+        return next.handle(req);
+    }
+}
