09067cd29ed11c251d8b6ebc132cf35a9d5edcc0
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-06-04 15:38:36 -0300

    angular contact service implementation

--- frontend/src/app/components/contacts/contacts.component.ts
+++ frontend/src/app/components/contacts/contacts.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { IContact } from '../../interfaces/contact.interface';
+import { ContactService } from '../../services/contacts.service';
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +8,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
 
+  Contacts: IContact[] = [];
+  
   @Output()
   onChat = new EventEmitter<string>();
+
+  constructor(private contact: ContactService) {}
+
+  ngOnInit(): void {
+      
+  }
 
   changeChat(value: string): void {
     this.onChat.emit(value);
---
+++ frontend/src/app/services/contacts.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IContact } from '../interfaces/contact.interface';
+import { Observable } from 'rxjs';
+import { IRequestContact } from '../interfaces/request-contact.interface';
+import { IContactRequest } from '../interfaces/contact-request.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ContactService {
+    
+    apiURL: string = '/api/contact';
+
+    constructor(private readonly http: HttpClient) {}
+
+    getContacts(): Observable<IContact[]> {
+        return this.http.get<IContact[]>(this.apiURL);
+    }
+
+    getRequest(): Observable<IRequestContact[]> {
+        return this.http.get<IRequestContact[]>(`${this.apiURL}/requests`);
+    }
+
+    sendRequest(username: string): Observable<void> {
+        let request: IContactRequest = { name: username };
+        
+        return this.http.post<void>(`${this.apiURL}/send`, request);
+    }
+
+    acceptRequest(username: string): Observable<void> {
+        let request: IContactRequest = { name: username };
+
+        return this.http.post<void>(`${this.apiURL}/accept`, request);
+    }
+}
